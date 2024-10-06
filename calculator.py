cfff4f1d6d0c7a109d95eb17281f0677483fc908
frostphex <cal1sahilt@gmail.com>
2024-10-06 19:51:50 +0530

    Update calculator class, add methods ask_choice() and exit(). These methods will asks user choice for operation and exit calculator

--- calculator.py
+++ calculator.py
@@ -8,16 +8,7 @@ class Calculator:
     """Initialize the numbers for operations."""
     self.num1 = num1
     self.num2 = num2
-    
-  def display_options(self):
-    """Display calculator options to the user"""
-    print("\nPlease select a calculator function to calculate: ")
-    print("1. Addition")
-    print("2. Subtraction")
-    print("3. Division")
-    print("4. Multiplication")
-    print("5. Exit")
-    
+
   def add(self):
     """Returns addition of two numbers."""
     return self.num1 + self.num2
@@ -38,7 +29,7 @@ class Calculator:
   def multiply(self):
     """Returns product of two numbers."""
     return self.num1 * self.num2
-  
+
   def calculate(self, choice):
     """Calculates the result of two numbers."""
     if choice == 1:
@@ -49,6 +40,29 @@ class Calculator:
       return self.divide()
     elif choice == 4:
       return self.multiply()
+    elif choice == 5:
+      return self.exit()
+    
+  def ask_choice(self):
+    """Asks user a operation choice, returns -1 if invalid."""
+    choice = int(input("> "))
+    if choice not in [1, 2, 3, 4, 5]:
+      print("Invalid choice. Please select a valid option.")
+      return -1
+    return choice
+    
+  def display_options(self):
+    """Display calculator options to the user"""
+    print("\nPlease select a calculator function to calculate: ")
+    print("1. Addition")
+    print("2. Subtraction")
+    print("3. Division")
+    print("4. Multiplication")
+    print("5. Exit")
+    
+  def exit(self):
+    confirm_exit = input("Do you really want to exit calculator? (yes/no): ")
+    return confirm_exit[0].lower()
 
 print(f"{'*' * 20} Calculator Project {'*' * 20}")
 print("Please enter two numbers:")
@@ -60,20 +74,17 @@ while True:
   calculator.display_options()
 
   try:
-    choice = int(input("> "))
-    if choice not in [1, 2, 3, 4, 5]:
+    choice = calculator.ask_choice()
+    if not choice:
       print("Invalid choice. Please select a valid option.")
       continue
   except ValueError:
     print("Error: Invalid input. Please enter a number.")
     continue
   else:
-    if choice == 5:
-      confirm_exit = input("Do you really want to exit calculator? (yes/no): ")
-      if confirm_exit[0].lower() == "y":
-        print("Exiting calculator. Have a great day!")
-        break
-      
-    result = calculator.calculate(choice)  
-    if result is not None:
+    result = calculator.calculate(choice) 
+    if result == 'y':
+      print("Exiting calculator. Have a great day!")
+      break
+    elif result is not None:
       print(f"\nResult: {result}")
