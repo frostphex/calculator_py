3af24b6da96c893acff7cdb174b2c3806a3212ee
frostphex <cal1sahilt@gmail.com>
2024-10-07 13:53:18 +0530

    Remove methods asking user for input, add them in user_interface.py. Refactor calculate method, use match case instead of if-else or dictionaries

--- calculator.py
+++ calculator.py
@@ -1,6 +1,7 @@
-# Calculator Project
+# /usr/bin/env python3
+# calculator.py - A calculator program to perform basic operations on numbers.
 
-# Basic functions - add, subtract, divide, multiply
+import user_interface as ui
 
 
 class Calculator:
@@ -24,9 +25,9 @@ class Calculator:
         try:
             result = self.num1 / self.num2
         except ZeroDivisionError:
+            result = None
             print("Error: Division by zero is not allowed.")
-        else:
-            return result
+        return result
 
     def multiply(self):
         """Returns product of two numbers."""
@@ -34,70 +35,18 @@ class Calculator:
 
     def calculate(self, choice):
         """Calculates the result of two numbers."""
-        operations = {
-            1: self.add,
-            2: self.subtract,
-            3: self.divide,
-            4: self.multiply,
-            5: self.exit,
-        }
-        return operations[choice]()
+        result = None
 
-    def ask_choice(self):
-        """Asks user a operation choice, returns -1 if invalid."""
-        try:
-            choice = int(input("> "))
-            if choice not in [1, 2, 3, 4, 5]:
-                print("Invalid choice. Please select a valid option.")
-                return -1
-            return choice
-        except ValueError:
-            print("Error: Invalid input. Please enter a number.")
-            return -1
+        match choice:
+            case 1:
+                result = self.add()
+            case 2:
+                result = self.subtract()
+            case 3:
+                result = self.divide()
+            case 4:
+                result = self.multiply()
+            case 5:
+                result = ui.confirm_exit()
 
-    def display_options(self):
-        """Display calculator options to the user"""
-        print("\nPlease select a calculator function to calculate: ")
-        print("1. Addition")
-        print("2. Subtraction")
-        print("3. Division")
-        print("4. Multiplication")
-        print("5. Exit")
-
-    def exit(self):
-        """Returns boolean if the user wants to exit the calculator. True if they want to exit the calculator."""
-        confirm_exit = input("Do you really want to exit calculator? (yes/no): ")
-        return confirm_exit[0].lower() == "y"
-
-
-def get_number(prompt):
-    """Helper function to get a valid number from the user."""
-    while True:
-        try:
-            return float(input(prompt))
-        except ValueError:
-            print("Error: Enter a valid number.")
-
-
-if __name__ == "__main__":
-    print(f"{'*' * 20} Calculator Project {'*' * 20}")
-    print("Please enter two numbers:")
-
-    num1 = get_number("First number: ")
-    num2 = get_number("Second number: ")
-
-    calculator = Calculator(num1, num2)
-
-    while True:
-        calculator.display_options()
-
-        choice = calculator.ask_choice()
-        if choice == -1:
-            continue
-
-        result = calculator.calculate(choice)
-        if result == True:
-            print("Exiting calculator. Have a great day!")
-            break
-        if result is not None:
-            print(f"\nResult: {result}")
+        return result
