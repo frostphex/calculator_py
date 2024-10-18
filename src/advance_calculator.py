096652d65185c2a730f02b9c21e969039477b2ca
frostphex <frostphex@proton.me>
2024-10-18 10:12:44 +0530

    Refactor find_* and calculate_* functions, add these functions as methods to AdvanceCalculator class.
    Make the find_* functions as methods in the  AdvanceCalculator class and
    make calculate_* functions as static methods.

--- src/advance_calculator.py
+++ src/advance_calculator.py
@@ -2,68 +2,55 @@ import math
 import random
 
 
-def find_square_root(number):
-    """Returns square root of a number."""
-    return math.sqrt(number)
+class AdvanceOperations:
+    """A class to perform advance mathematical operations."""
 
+    def __init__(self, number):
+        """Initialize with a number."""
+        self.number = number
 
-def find_absolute_value(number):
-    """Returns absolute value of a number."""
-    return abs(number)
+    def find_square_root(self):
+        """Returns square root of a number."""
+        return math.sqrt(self.number)
 
+    def find_absolute_value(self):
+        """Returns absolute value of a number."""
+        return abs(self.number)
 
-def find_sine(number):
-    """Returns sine of a number"""
-    return math.sin(number)
+    def find_sine(self):
+        """Returns sine of a number"""
+        return math.sin(self.number)
 
+    def find_cosine(self):
+        """Returns cosine of a number"""
+        return math.cos(self.number)
 
-def find_cosine(number):
-    """Returns cosine of a number"""
-    return math.cos(number)
+    def find_tangent(self):
+        """Returns tan of a number"""
+        return math.tan(self.number)
 
+    @staticmethod
+    def calculate_exponentiation(base, power):
+        """Returns a number raised to the power."""
+        return math.pow(base, power)
 
-def find_tangent(number):
-    """Returns tan of a number"""
-    return math.tangent(number)
+    @staticmethod
+    def calculate_log(number, base):
+        """Returns a log of a number."""
+        if number <= 0 or base <= 0:
+            raise ValueError("Number and base must be greater than zero.")
+        return math.log(number, base)
 
-
-def calculate_exponentiation():
-    """Returns a number raised to the power."""
-    print("Enter base:")
-    base = float(input("> "))
-
-    print("Enter power:")
-    power = float(input("> "))
-    return math.pow(base, power)
-
-
-def calculate_log():
-    """Returns a log of a number."""
-    print("Enter number:")
-    number = float(input("> "))
-
-    print("Enter base:")
-    base = float(input("> "))
-    return math.log(number, base)
-
-
-def calculate_factorial():
-    """Returns the factorial of a number."""
-    print("Enter the number:")
-    try:
-        number = int(input("> "))
-    except ValueError:
-        print("Please enter a number without decimal.")
-    else:
+    @staticmethod
+    def calculate_factorial(number):
+        """Returns the factorial of a number."""
+        if number < 0:
+            raise ValueError("Number must be non-negative.")
         return math.factorial(number)
 
-
-def generate_random_number():
-    """Returns a random number."""
-    try:
-        min_range = int(input("Enter a minimum range:\n> "))
-        max_range = int(input("Enter a maximum range:\n> "))
-    except ValueError:
-        print("Please enter a number without decimal.")
-    else:
+    @staticmethod
+    def generate_random_number(min_range, max_range):
+        """Returns a random number."""
+        if min_range > max_range:
+            raise ValueError("Minimum range should be less than maximum range.")
         return random.randint(min_range, max_range)
